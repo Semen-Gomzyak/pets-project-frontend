6290470d1d468c5b75eb0a9b4d763eaf2920d22b
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-02-20 21:12:58 +0100

    small fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
 import { store } from './redux/store';
+
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App/App';
 import './index.css';
