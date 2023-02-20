8521ed84aaee0e74a9030280a0cd812f6d9a3903
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-02-20 22:59:08 +0100

    small fixed

--- src/services/PrivateRoute.jsx
+++ src/services/PrivateRoute.jsx
@@ -1,8 +1,9 @@
 // import { useSelector } from 'react-redux';
-// import { Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 // import { selectIsAuth } from 'redux/Auth/authSelectors';
 
-// export const PrivateRoute = ({ children }) => {
-//   const isLoggedIn = useSelector(selectIsAuth);
-//   return isLoggedIn ? children : <Navigate to="/login" />;
-// };
+export const PrivateRoute = ({ children }) => {
+  // const isLoggedIn = useSelector(selectIsAuth);
+  const isLoggedIn = null
+  return isLoggedIn ? children : <Navigate to="/login" />;
+};
