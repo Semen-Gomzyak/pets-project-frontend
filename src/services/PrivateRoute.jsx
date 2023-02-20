2dcd62269a0bda3e2f02385721817b2332e14f41
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-02-20 23:00:53 +0100

    change private router

--- src/services/PrivateRoute.jsx
+++ src/services/PrivateRoute.jsx
@@ -4,6 +4,6 @@ import { Navigate } from 'react-router-dom';
 
 export const PrivateRoute = ({ children }) => {
   // const isLoggedIn = useSelector(selectIsAuth);
-  const isLoggedIn = null
+  const isLoggedIn = null;
   return isLoggedIn ? children : <Navigate to="/login" />;
 };
