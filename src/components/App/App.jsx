c3653583f0516122e8b7ae11f92a63314ec5c13c
Roman Vildanov <vildanovv.rr@gmail.com>
2023-02-20 23:30:13 +0200

    add some styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -44,9 +44,7 @@ export const App = () => {
 
       <Route path="/profile" element={<Profile />} />
 
-      <Route path="notices" element={<NoticesPage />}>
-
-      </Route>
+      <Route path="notices" element={<NoticesPage />}></Route>
     </Routes>
   );
 };
