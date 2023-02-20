a43f38e00edbb2200a56fa3e40a63ff33c84253e
Homziak Semen <91370041+Semen-Gomzyak@users.noreply.github.com>
2023-02-20 16:27:43 +0100

    Update PublicRoute.jsx

--- src/services/PublicRoute.jsx
+++ src/services/PublicRoute.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { selectIsAuth } from 'redux/auth/authSelectors';
+import { selectIsAuth } from 'redux/Auth/authSelectors';
 
 export const PublicRoute = ({ children, restricted = false }) => {
   const isLoggedIn = useSelector(selectIsAuth);
