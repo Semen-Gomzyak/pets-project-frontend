17722e90fef55a1f80edde850ac52d35ad0a0a26
PavloMykhailovskyi <100272784+PavloMykhailovskyi@users.noreply.github.com>
2023-02-22 00:33:20 +0200

    add formik

---
+++ src/validations/SecondRegisterValidation.js
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+
+export const SecondRegisterFormSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Name is required')
+    .matches(/^[a-zA-Zа-яА-Я\s]*$/, 'Please enter a valid name'),
+  city: Yup.string()
+    .required('City is required')
+    .matches(
+      /^[a-zA-Zа-яА-Я\s]*,\s*[a-zA-Zа-яА-Я\s]*$/,
+      'Enter the city and region in the format "City, State"'
+    ),
+  mobilePhone: Yup.string()
+    .required('Phone is required')
+    .matches(
+      /^\+380\d{9}$/,
+      'Enter your mobile number in the format "+380*********"'
+    ),
+});
