efe44b4d17b012d1dcda7a5b822dde36e6a0a75d
Roman Vildanov <vildanovv.rr@gmail.com>
2023-02-22 19:18:11 +0200

    add styles to account Botton

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -1,11 +1,11 @@
 import { UserNavDiv, UserNavLink, UserNavIcon } from './UserNav.styled';
-// import AccountIcon from '';
+import accountIcon from '../../images/userAndPets/accountIcon.svg';
 
 export const UserNav = () => {
   return (
     <UserNavDiv>
       <UserNavLink to="/user">
-        <UserNavIcon src="/" alt="account" />
+        <UserNavIcon src={accountIcon} alt="account" />
         Account
       </UserNavLink>
     </UserNavDiv>
